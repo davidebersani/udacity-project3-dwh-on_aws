b037ef8d19e5e9e4bc49cc03efd95006dfb60c2c
Davide Bersani <davide.bersani@enel.com>
2021-08-11 11:04:24 +0200

    Added sql queries (create, drop, copy, insert)

--- sql_queries.py
+++ sql_queries.py
@@ -6,64 +6,162 @@ config.read("dwh.cfg")
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_event"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_song"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create = """
+CREATE TABLE IF NOT EXISTS staging_event (
+    event_id SERIAL PRIMARY KEY,
+    artist text,
+    auth text
+    firstName text,
+    gender char,
+    itemInSession integer,
+    lastName text,
+    length numeric,
+    level varchar(10),
+    location text,
+    method varchar(10),
+    page text,
+    registration bigint,
+    sessionId integer,
+    song text,
+    status integer,
+    ts bigint,
+    userAgent text,
+    userId integer
+);
 """
 
 staging_songs_table_create = """
+CREATE TABLE IF NOT EXISTS staging_song (
+    song_id SERIAL PRIMARY KEY,
+    num_songs integer,
+    artist_id text,
+    artist_latitude text,
+    artist_longitude text,
+    artist_location text,
+    artist_name text,
+    song_id text,
+    title text,
+    duration numeric,
+    year integer
+)
 """
 
 songplay_table_create = """
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id BIGSERIAL PRIMARY KEY,
+    start_time timestamp NOT NULL,
+    user_id int NOT NULL,
+    level varchar,
+    song_id varchar NOT NULL,
+    artist_id varchar NOT NULL,
+    session_id int,
+    location varchar,
+    user_agent varchar
+    );
 """
 
 user_table_create = """
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY,
+    first_name varchar,
+    last_name varchar,
+    gender char,
+    level varchar
+    );
 """
 
 song_table_create = """
+CREATE TABLE IF NOT EXISTS songs (
+    song_id varchar PRIMARY KEY,
+    title varchar,
+    artist_id varchar,
+    year int,
+    duration numeric
+    );
 """
 
 artist_table_create = """
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id varchar PRIMARY KEY,
+    name varchar,
+    location varchar,
+    latitude numeric,
+    longitude numeric
+    );
 """
 
 time_table_create = """
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday int
+    );
 """
 
 # STAGING TABLES
 
 staging_events_copy = (
     """
+    COPY staging_event FROM {s3_file_path}
+    CREDENTIALS 'aws_iam_role={role_arn}'
+    json '{json_path}';
+
 """
-).format()
+).format(s3_file_path=config.get("LOG_DATA"), role_arn=config.get("ARN"), json_path=config.get("LOG_JSONPATH"))
 
 staging_songs_copy = (
     """
+    COPY staging_song FROM {s3_file_path}
+    CREDENTIALS 'aws_iam_role={role_arn}'
+    json 'auto';
 """
-).format()
+).format(s3_file_path=config.get("SONG_DATA"), role_arn=config.get("ARN"))
 
 # FINAL TABLES
 
 songplay_table_insert = """
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """
 
 user_table_insert = """
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE
+SET level = EXCLUDED.level
 """
 
 song_table_insert = """
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING
 """
 
 artist_table_insert = """
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
 """
 
+
 time_table_insert = """
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING
 """
 
 # QUERY LISTS
