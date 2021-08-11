5b091e16be6d522a268a99b18b8b895633d67586
Davide Bersani <davide.bersani@enel.com>
2021-08-11 12:42:36 +0200

    Added log

--- etl.py
+++ etl.py
@@ -1,8 +1,11 @@
 import configparser
+import logging
 
 import psycopg2
 
 from sql_queries import copy_table_queries, insert_table_queries
+
+logging.basicConfig(level=logging.INFO)
 
 
 def load_staging_tables(cur, conn):
@@ -21,11 +24,16 @@ def main():
     config = configparser.ConfigParser()
     config.read("dwh.cfg")
 
+    logging.info("Connecting to Redshift cluster...")
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config["CLUSTER"].values()))
     cur = conn.cursor()
 
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+    logging.info("Loading staging tables...")
+    # load_staging_tables(cur, conn)
+    print(copy_table_queries[1])
+    cur.execute(copy_table_queries[1])
+    conn.commit()
+    # insert_tables(cur, conn)
 
     conn.close()
 
